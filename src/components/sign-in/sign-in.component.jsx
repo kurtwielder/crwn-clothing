b80263abe35938b619c19925c32249950811cb8f
Kurt Wielder <kurtwielder@gmail.com>
2020-01-10 21:06:16 +0000

    ''

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import './sign-in.styles.scss';
 
+import FormInput from '../form-input/form-input.component';
+
 class SignIn extends React.Component {
     constructor(props) {
         super(props);
@@ -10,14 +12,43 @@ class SignIn extends React.Component {
             email: '',
             password: ''
         }
-    },
+    };
+
+    handleChange = event => {
+        const { value, name } = event.target;
+        this.setState({ [name]: value }); 
+    };
+
+    handleSubmit = () => {
+
+    };
 
     render() {
         return (
             <div className='sign-in'>
                 <h2>I already have an account</h2>
                 <span>Sign in with your email and password</span>
+
+                <form onSubmit={this.handleSubmit}>
+                    <FormInput 
+                        name='email' 
+                        type='email' 
+                        handleChange={this.handleChange} 
+                        value={this.state.email} 
+                        label='email'
+                        required />
+                    <FormInput 
+                        name='password' 
+                        type='password' 
+                        handleChange={this.handleChange} 
+                        value={this.state.password} 
+                        label='password'
+                        required />
+                </form>
+
             </div>
         )
     }    
 }
+
+export default SignIn;
